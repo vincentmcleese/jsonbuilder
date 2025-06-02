5f397a92d444a06eec819ea86a1f1abbec412522
Vincent Mcleese <mcleesevj@gmail.com>
2025-06-02 09:58:41 +0200

    Convert Next.js config to .mjs format

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   transpilePackages: ['react-markdown', 'remark-gfm'],
   webpack: (config) => {
     config.cache = false;
