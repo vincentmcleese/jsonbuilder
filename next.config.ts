5f5d2c608442e0d347491ebdb99479d30224c185
Vincent Mcleese <mcleesevj@gmail.com>
2025-06-02 09:50:29 +0200

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['react-markdown', 'remark-gfm']
+  transpilePackages: ['react-markdown', 'remark-gfm'],
+  webpack: (config) => {
+    config.cache = false;
+    return config;
+  }
 };
 
 export default nextConfig;
