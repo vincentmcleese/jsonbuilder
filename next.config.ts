28b7badae04bce5897beb4f6fd137f2bcee446d8
Vincent Mcleese <mcleesevj@gmail.com>
2025-06-02 09:45:10 +0200

    Update Next.js configuration for module transpilation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ['react-markdown', 'remark-gfm']
 };
 
 export default nextConfig;
